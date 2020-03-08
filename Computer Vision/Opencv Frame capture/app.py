462b86bfbb740268f2e6de6d1dcbcc835d6f4d9e
Ashwin Raikar <ashwin.raikar77@gmail.com>
2020-03-08 19:03:41 +0530

    Update and rename app.py to app_delay.py

--- Computer Vision/Opencv Frame capture/app.py
+++
@@ -1,9 +0,0 @@
-import capture_frame
-import cv2
-
-a, output = capture_frame.capture_delay('My Image.jpg', 120)
-cv2.imshow(output, a)
-cv2.waitKey(2550)
-cv2.destroyAllWindows()
-
-print("Program ran successfully!")
